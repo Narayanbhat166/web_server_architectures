4ee30bbce23bb9a4509414670bf689eeeec0d0a6
Narayan Bhat <narayan.bhat@juspay.in>
2023-10-15 16:18:58 +0530

    refactor: move util fns to library

--- src/bin/mio_async.rs
+++ src/bin/mio_async.rs
@@ -1,16 +1,10 @@
-use std::{
-    collections::HashMap,
-    fs,
-    future::Ready,
-    io::{BufRead, BufReader, Write},
-    thread::sleep,
-    time::Duration,
-};
+use std::{collections::HashMap, io::Write};
 
 use mio::{
     net::{TcpListener, TcpStream},
     Events, Interest, Poll, Token,
 };
+use server::handle_mio_connection;
 
 fn main() {
     let address = "127.0.0.1:6969";
@@ -34,7 +28,7 @@ fn main() {
             if event.is_readable() {
                 let mut stream = listener.accept().unwrap().0;
 
-                let response = handle_connection(&mut stream);
+                let response = handle_mio_connection(&mut stream);
                 counter += 1;
 
                 println!("writable event registered for token {counter}");
@@ -66,42 +60,4 @@ fn main() {
             }
         }
     }
-}
-
-fn handle_connection(stream: &mut TcpStream) -> String {
-    // let buf_reader = BufReader::new(stream);
-
-    // The first line of the request contains data in the below format
-    // Method Request-URI HTTP-Version CRLF
-    // ex: GET /2 HTTP/1.1
-    // let http_request = buf_reader
-    //     .lines()
-    //     .take(1)
-    //     .next()
-    //     .map(|line| {
-    //         println!("Request {line:?}");
-    //         line
-    //     })
-    //     .transpose()
-    //     .ok()
-    //     .flatten()
-    //     .unwrap_or_default();
-
-    // let sleep_time = http_request
-    //     .split(" ")
-    //     .nth(1)
-    //     .map(|uri| &uri[1..])
-    //     .filter(|stripped_uri| !stripped_uri.is_empty())
-    //     .map(|sleep_time| sleep_time.parse::<u16>().unwrap())
-    //     .unwrap_or(100);
-
-    // std::thread::sleep(Duration::from_millis(sleep_time.into()));
-
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("static/index.html").unwrap();
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    response
 }
---
+++ src/lib.rs
@@ -0,0 +1,84 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net,
+    time::Duration,
+};
+
+pub fn handle_mio_connection(_stream: &mut mio::net::TcpStream) -> String {
+    // let buf_reader = BufReader::new(stream);
+
+    // The first line of the request contains data in the below format
+    // Method Request-URI HTTP-Version CRLF
+    // ex: GET /2 HTTP/1.1
+
+    // let http_request = buf_reader
+    //     .lines()
+    //     .take(1)
+    //     .next()
+    //     .map(|line| {
+    //         println!("Request {line:?}");
+    //         line
+    //     })
+    //     .transpose()
+    //     .ok()
+    //     .flatten()
+    //     .unwrap_or_default();
+
+    // let sleep_time = http_request
+    //     .split(" ")
+    //     .nth(1)
+    //     .map(|uri| &uri[1..])
+    //     .filter(|stripped_uri| !stripped_uri.is_empty())
+    //     .map(|sleep_time| sleep_time.parse::<u16>().unwrap());
+
+    // if let Some(sleep_time) = sleep_time {
+    //     std::thread::sleep(Duration::from_millis(sleep_time.into()))
+    // }
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("static/index.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    response
+}
+
+pub fn handle_connection(mut stream: net::TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    // The first line of the request contains data in the below format
+    // Method Request-URI HTTP-Version CRLF
+    // ex: GET /2 HTTP/1.1
+    let http_request = buf_reader
+        .lines()
+        .take(1)
+        .next()
+        .map(|line| {
+            println!("Request {line:?}");
+            line
+        })
+        .transpose()
+        .ok()
+        .flatten()
+        .unwrap_or_default();
+
+    let sleep_time = http_request
+        .split(" ")
+        .nth(1)
+        .map(|uri| &uri[1..])
+        .filter(|stripped_uri| !stripped_uri.is_empty())
+        .map(|sleep_time| sleep_time.parse::<u16>().unwrap())
+        .unwrap_or(500);
+
+    std::thread::sleep(Duration::from_millis(sleep_time.into()));
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("static/index.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
