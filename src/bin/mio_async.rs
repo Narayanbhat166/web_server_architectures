6792b6b249ecd05cadf118eaa2d8d68519a85d48
Narayanbhat166 <bhatj1964@gmail.com>
2023-11-15 12:42:55 +0530

    refactor: add colorful logs

--- src/bin/mio_async.rs
+++ src/bin/mio_async.rs
@@ -24,7 +24,10 @@ fn get_unix_timestamp() -> u128 {
 }
 
 fn main() {
-    env_logger::builder().filter_level(log::LevelFilter::Debug);
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
+
     let address = "127.0.0.1:6969";
     let mut listener = TcpListener::bind(address.parse().unwrap()).unwrap();
     log::info!("Listening on http://{address}");
@@ -48,17 +51,22 @@ fn main() {
         poll.poll(&mut events, Some(Duration::from_millis(10)))
             .unwrap();
         for event in &events {
-            println!("{event:?}");
+            log::info!("Received an event");
+            log::debug!("Event details {event:?}");
+
             match event.token() {
                 Token(0) => loop {
+                    log::info!("Might be a Server event");
                     let mut stream = match listener.accept() {
-                        Ok((stream, _)) => stream,
+                        Ok((stream, _)) => {
+                            log::info!("Accepted a connection");
+                            stream
+                        }
                         Err(err) => {
-                            eprint!("{err:?}");
+                            log::error!("{err:?}");
                             break;
                         }
                     };
-                    println!("Read {counter}");
 
                     stream
                         .register(poll.registry(), Token(counter), Interest::READABLE)
@@ -69,33 +77,35 @@ fn main() {
                 },
                 _ => {
                     if event.is_readable() {
+                        log::info!("Readable event");
+
                         let token = event.token().0;
                         let mut stream = sockets_hm.get_mut(&token).unwrap();
-                        let response = handle_mio_connection(&mut stream);
+                        let (response, sleep_time) = handle_mio_connection(&mut stream);
 
-                        // log::error!("writable event registered for token {token} after 1 seconds");
-                        let expire_timeout = get_unix_timestamp() + 500;
+                        let expire_timeout = get_unix_timestamp() + u128::from(sleep_time);
                         timer_queue.push_back((expire_timeout, token));
 
                         responses.insert(token, response);
                     } else if event.is_writable() {
+                        log::info!("Writable event");
+
                         let token = event.token().0;
-                        // prinstln!("writable event triggered for token {token}");
 
                         let stream = sockets_hm.get_mut(&token).unwrap();
                         let response = responses.get(&token).unwrap();
 
                         if !event.is_write_closed() {
-                            // println!("Writing to stream");
                             stream
                                 .write_all(response.as_bytes())
                                 .expect("Could not write to stream");
+                            log::info!("Writing Done");
+
                             stream.flush().unwrap();
 
                             poll.registry().deregister(stream).unwrap();
                             sockets_hm.remove(&token);
                             responses.remove(&token);
-                            // println!("Writing done!");
                         }
                     }
                 }
@@ -106,15 +116,18 @@ fn main() {
             let current_timestamp = get_unix_timestamp();
             while let Some((event_timestamp, token)) = timer_queue.front() {
                 if event_timestamp < &current_timestamp {
-                    println!("Expired {token}");
+                    log::info!("Timer expired for token {token}");
+
                     // Timer has expired, register interest for writing to the stream
                     let (_, token) = timer_queue.pop_front().unwrap();
                     let tcp_stream = sockets_hm.get_mut(&token).unwrap();
+
+                    log::info!("Registering write interest");
                     tcp_stream
                         .reregister(poll.registry(), Token(token), Interest::WRITABLE)
                         .unwrap();
                 } else {
-                    println!("Not expired {event_timestamp} {token}");
+                    // println!("Not expired {event_timestamp} {token}");
                     // later events will not have been expired as per the assuption that
                     // default timeout that this application supports is 1s
                     break;
--- src/lib.rs
+++ src/lib.rs
@@ -5,36 +5,34 @@ use std::{
     time::Duration,
 };
 
-pub fn handle_mio_connection(stream: &mut mio::net::TcpStream) -> String {
-    // let buf_reader = BufReader::new(stream);
+pub fn handle_mio_connection(stream: &mut mio::net::TcpStream) -> (String, u16) {
+    let buf_reader = BufReader::new(stream);
 
     // The first line of the request contains data in the below format
     // Method Request-URI HTTP-Version CRLF
     // ex: GET /2 HTTP/1.1
 
-    // let http_request = buf_reader
-    //     .lines()
-    //     .take(1)
-    //     .next()
-    //     .map(|line| {
-    //         println!("Request {line:?}");
-    //         line
-    //     })
-    //     .transpose()
-    //     .ok()
-    //     .flatten()
-    //     .unwrap_or_default();
+    let http_request = buf_reader
+        .lines()
+        .take(1)
+        .next()
+        .map(|line| {
+            log::info!("Request {line:?}");
+            line
+        })
+        .transpose()
+        .ok()
+        .flatten()
+        .unwrap_or_default();
 
-    // let sleep_time = http_request
-    //     .split(" ")
-    //     .nth(1)
-    //     .map(|uri| &uri[1..])
-    //     .filter(|stripped_uri| !stripped_uri.is_empty())
-    //     .map(|sleep_time| sleep_time.parse::<u16>().unwrap());
+    let sleep_time = http_request
+        .split(" ")
+        .nth(1)
+        .map(|uri| &uri[1..])
+        .filter(|stripped_uri| !stripped_uri.is_empty())
+        .map(|sleep_time| sleep_time.parse::<u16>().unwrap());
 
-    // if let Some(sleep_time) = sleep_time {
-    //     std::thread::sleep(Duration::from_millis(sleep_time.into()))
-    // }
+    log::warn!("Sleep for {sleep_time:?} ms");
 
     let status_line = "HTTP/1.1 200 OK";
     let contents = fs::read_to_string("static/index.html").unwrap();
@@ -42,7 +40,7 @@ pub fn handle_mio_connection(stream: &mut mio::net::TcpStream) -> String {
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    response
+    (response, sleep_time.unwrap_or(1000))
 }
 
 pub fn handle_connection(mut stream: net::TcpStream) {
